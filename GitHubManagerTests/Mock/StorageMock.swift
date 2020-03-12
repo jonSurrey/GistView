9330852ff9526f42a2e30db39813d15173d9a239
Jonathan Martins <jon.martins@outlook.com>
2020-03-12 01:26:42 -0300

    - Updated StorageMock

--- GitHubManagerTests/Mock/StorageMock.swift
+++ GitHubManagerTests/Mock/StorageMock.swift
@@ -15,30 +15,33 @@ class StorageMock: StorageDelegate {
     private var favorites:[Gist] = []
     
     func clearCache() {
-        
+        cache = []
     }
     
     func loadCache() -> [Gist] {
-        
+        return cache
     }
     
-    func addToCache(_ gist: [Gist]) {
-        
+    func addToCache(_ gist:[Gist]) {
+        cache.append(gist)
     }
-    
+
     func loadFavorites() -> [Gist] {
-        
+        return favorites
+    }
+
+    func addToFavorite(_ gist:Gist) {
+        favorites.append(gist)
+    }
+
+    func removeFromFavorite(_ gist:Gist) {
+        favorites.removeAll(where: {
+            $0.id == gist.id
+        })
     }
     
-    func addToFavorite(_ gist: Gist) {
-        
-    }
-    
-    func removeFromFavorite(_ gist: Gist) {
-        
-    }
-    
-    func isGistFavorite(_ gist: Gist) -> Bool {
-        
+    func isGistFavorite(_ gist:Gist) -> Bool {
+        let isFavorite = favorites.first(where: { $0.id == gist.id })
+        return isFavorite != nil ? true : false
     }
 }
